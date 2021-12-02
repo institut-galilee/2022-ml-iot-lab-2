adbe748de026092a212ab579a41d115e38499fdc
Massinissa <hamidimassinissa@gmail.com>
2021-12-02 20:19:12 +0100

    Update get_data.sh
    Now, we only download a sample (subset of the SHL data) and from Google drive.

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-data_dir=./data/
+data_dir=./data
 
 # This list corresponds to the files used during 2020 SHL challenge
 # lst=(
@@ -10,20 +10,22 @@ data_dir=./data/
 #     "http://www.shl-dataset.org/wp-content/uploads/SHLChallenge2020/challenge-2020-train_hand.zip"
 #     "http://www.shl-dataset.org/wp-content/uploads/SHLChallenge2020/challenge-2020-validation.zip"
 #     "http://www.shl-dataset.org/wp-content/uploads/SHLChallenge2020/challenge-2020-test.zip"
+#     "https://cloud.lipn.univ-paris13.fr/index.php/s/YY3AJzoc8QmnDc9/download"
+#     "https://cloud.lipn.univ-paris13.fr/index.php/s/nNGKXZnoQdaGkt7/download"
 # )
 
 # The following files, one for train and one for validation, will be used during M2 course
 lst=(
-    "https://cloud.lipn.univ-paris13.fr/index.php/s/YY3AJzoc8QmnDc9/download"
-    "https://cloud.lipn.univ-paris13.fr/index.php/s/nNGKXZnoQdaGkt7/download"
+    "17y9iMIdW7cuYukMhH7wepFFZ0hJqHWHQ"
 )
 
 echo "[SHL Challenge] Downloading the shldataset. Depending on your bandwidth, this may take a little while."
 
 for l in "${lst[@]}"
 do
-    echo "[SHL Challenge] downloading from " $l " into " $data_dir " ..."
-    wget --content-disposition $l -N -P $data_dir/  # -N overwrite only if there is a new version in the server
+    echo "[SHL Challenge] downloading " $l " into " $data_dir " ..."
+    # wget --content-disposition $l -N -P $data_dir/  # -N overwrite only if there is a new version in the server
+    wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id='$l -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id="$l -O $data_dir/sample.zip -N -P $data_dir && rm -rf /tmp/cookies.txt
 done
 
 if [ $? == "0" ]
